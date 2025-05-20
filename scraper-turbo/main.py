48a4eb4b5728c253ab124956b12449279ea58dc1
Kamran Gayibov <qayibovkamran@gmail.com>
2025-05-20 11:25:01 +0400

    Add function to retrieve and save specific car information from Turbo.az

--- scraper-turbo/main.py
+++ scraper-turbo/main.py
@@ -21,5 +21,17 @@ def main():
     with open("result.html", "w", encoding="utf-8") as file:
         file.write("\n".join(str(item) for item in all))
 
+def get_specific_car_info(car_id: str):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+    base_car_url = f"https://turbo.az/autos/{car_id}"
+    response = requests.get(base_car_url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    print(soup.prettify())
+    with open("car_info.html", "w", encoding="utf-8") as file:
+        file.write(soup.prettify())
+
 if __name__ == "__main__":
     main()
+    get_specific_car_info("9453404")
